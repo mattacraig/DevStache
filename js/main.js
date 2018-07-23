55e1e4620436bb8f18a20460410d2813f539ec5a
mattacraig <ma.craig006@gmail.com>
2018-07-23 12:33:49 -0400

    add height transition to nav

--- js/main.js
+++ js/main.js
@@ -1,16 +1,36 @@
 ---
 ---
-window.addEventListener('scroll', () => {
-  let scrolled = window.pageYOffset || document.body.scrollTop,
+
+const addOpacity = () => {
+	let scrolled 		= window.pageYOffset || document.body.scrollTop,
       overlayOpacity;
-  const overlay = document.querySelector('.overlay'),
-        fadeUntil = overlay.offsetHeight;
-  
-  if(scrolled <= fadeUntil) {
-    overlayOpacity = (scrolled/fadeUntil) * 1;
-  }
-  
+      
+	const overlay 	= document.querySelector('.overlay');
+	const fadeUntil = overlay.offsetHeight;
+	
+	if(scrolled <= fadeUntil) {
+  	overlayOpacity = (scrolled/fadeUntil) * 1;
+		}
+
   overlay.style.opacity = overlayOpacity;
-});
+};
+
+const reduceNavHeight = () => {
+	let scrolled = window.pageYOffset || document.body.scrollTop;
+
+	const header 			 = document.querySelector('header'),
+				nav 				 = document.querySelector('nav'),
+				headerHeight = header.offsetHeight;
+
+	if (scrolled >= headerHeight) {
+		nav.classList.add('nav-scrolled');
+	}else if (scrolled < headerHeight) {
+		nav.classList.remove('nav-scrolled');
+	}
+};
+
+
+window.addEventListener('scroll', addOpacity, false);
+window.addEventListener('scroll', reduceNavHeight, false);
 
 console.log('this works');
